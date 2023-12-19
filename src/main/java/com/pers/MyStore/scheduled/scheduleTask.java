c16c8e43f9213acd9b10a0813e7784cb9e57a337
Hn1127 <154227478+Hn1127@users.noreply.github.com>
2023-12-19 21:03:11 +0800

    Delete src/main directory

--- src/main/java/com/pers/MyStore/scheduled/scheduleTask.java
+++
@@ -1,5 +0,0 @@
-package com.pers.MyStore.scheduled;
-
-public class scheduleTask {
-
-}
--- src/main/resources/static/js/ShowHiddenForm.js
+++
@@ -1,9 +0,0 @@
-// JavaScript 函数，用于显示弹窗 
-function openForm() {
-document.getElementById("Hidden").style.display = "block"; 
-} 
-// JavaScript 函数，用于关闭弹窗 
-function closeForm() {
-document.getElementById("Hidden").style.display = "none";
-}
-
